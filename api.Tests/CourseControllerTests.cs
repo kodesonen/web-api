2c2cb6e8cd43089350db52165f53043eb6e607c5
magnus2142 <magnus.bredeli@hotmail.com>
2022-04-02 15:32:48 +0200

    Merged all lose branches. Managed to pull into production, but fixed it and development should now contain all branches

--- api.Tests/CourseControllerTests.cs
+++ api.Tests/CourseControllerTests.cs
@@ -6,24 +6,39 @@ using System.Collections.Generic;
 using Newtonsoft.Json;
 using FluentAssertions;
 using api.Models;
+using System.Security.Claims;
 using Xunit;
 
 
-namespace api.Tests
+namespace api.Tests.IntegrationTest
 {
-    public class CourseControllerTests : IntegrationTest
+    public class CourseControllerTests : IClassFixture<AppInstance>
     {
+
+        private readonly AppInstance _instance;
+
+        public CourseControllerTests(AppInstance instance)
+        {
+            _instance = instance;
+        }
+
         [Fact]
-        public async Task Getcourses_WithoutAnyCourses_ReturnsEmptyResponse()
+        public async Task Getcourses_WithoutTwoCourses_ReturnsTwoCourses()
         {
             // Arrange
+            var _httpClient = _instance
+            .AuthenticatedInstance(new Claim(ClaimTypes.Role, "Admin"))
+            .CreateClient(new()
+            {
+                AllowAutoRedirect = false
+            });
 
             // Act
             var response = await _httpClient.GetAsync("/api/Course");
 
             // Assert
             response.StatusCode.Should().Be(HttpStatusCode.OK);
-            (await response.Content.ReadAsAsync<List<Course>>()).Should().BeEmpty();
+            (await response.Content.ReadAsAsync<List<Course>>()).Should().HaveCount(2);
         }
     }
 }
